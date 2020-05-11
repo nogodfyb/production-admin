4f74f6edbddc6baa77a3c1dbceaf2bcb500aff47
nogodfyb <120626481@qq.com>
2020-05-11 11:26:01 +0800

    fix interceptor and cross config conflicts

--- src/main/java/com/fyb/production/config/WebAppConfig.java
+++ src/main/java/com/fyb/production/config/WebAppConfig.java
@@ -2,7 +2,6 @@ package com.fyb.production.config;
 
 import com.fyb.production.interceptor.SessionInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +18,7 @@ public class WebAppConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/production/user/login");
     }
 
+/*
     @Override
     //重写父类提供的跨域请求处理的接口
     public void addCorsMappings(CorsRegistry registry) {
@@ -31,10 +31,10 @@ public class WebAppConfig implements WebMvcConfigurer {
                 //放行哪些原始域(请求方式)
                 .allowedMethods("GET","POST", "PUT", "DELETE")
                 //放行哪些原始域(头部信息)
-                .allowedHeaders("*")
+                .allowedHeaders("*");
                 //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-                .exposedHeaders("Header1", "Header2");
+               // exposedHeaders("Header1", "Header2");
     }
-
+*/
 
 }
