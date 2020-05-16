6fa2a4399ca8b258f5d7952b19fd94f629788242
nogodfyb <120626481@qq.com>
2020-05-16 23:41:14 +0800

    fix patterns

--- src/main/java/com/fyb/production/config/WebAppConfig.java
+++ src/main/java/com/fyb/production/config/WebAppConfig.java
@@ -14,7 +14,7 @@ public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 设置拦截的路径、不拦截的路径、优先级等等
-        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**")
+        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/production/**")
                 .excludePathPatterns("/production/user/login");
     }
 
